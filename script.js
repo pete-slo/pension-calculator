0d38be764042dc22ce897773047c37a555856894
PeterS <peters@MHAGA099L.admin.gov.ky>
2025-08-06 13:02:06 -0500

    percent column

--- script.js
+++ script.js
@@ -156,13 +156,13 @@ accumulation.forEach(row => {
   // Drawdown table
   
 	output += "<h3>Drawdown Phase</h3>";
-  output += "<table class='drawdown-table' border='1'><tr><th class='year'>Year</th><th class='age'>Age</th><th class='balance'>Balance at Start (CI$)</th><th>%</th><th class='drawdown'>Max Drawdown (CI$)</th><th class='growth'>Growth (CI$)</th></tr>";
+  output += "<table class='drawdown-table' border='1'><tr><th class='year'>Year</th><th class='age'>Age</th><th class='balance'>Balance at Start (CI$)</th><th class='percentage'>Max %</th><th class='drawdown'>Max Drawdown (CI$)</th><th class='growth'>Growth (CI$)</th></tr>";
 drawdown.forEach(row => {
 	  output += `<tr>
 		<td class="year centered">${row.year}</td>
     <td class="age centered">${row.age}</td>
     <td class="balance">${Number(row.balance).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</td>
-    <td>${row.percentage}</td>
+    <td class="percentage">${row.percentage}</td>
     <td class="drawdown">${Number(row.drawdown).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</td>
     <td class="growth">${Number(row.growth).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</td>
 	  </tr>`;
